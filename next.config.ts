60cca4d446ff4cab3a021390e61db32d98ad436a
Prateek kumar <prateeksinghkamalpura@gmail.com>
2025-09-16 06:00:53 +0530

    vercel issue fixed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Netlify ke liye static export
-  output: 'export',
-
   typescript: {
     ignoreBuildErrors: true,
   },
