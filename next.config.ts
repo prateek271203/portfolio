0506b2a3d6c9f5b07d41b371b1d8f22a84142c52
Prateek kumar <prateeksinghkamalpura@gmail.com>
2025-09-16 05:46:01 +0530

    fix netlify issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Netlify ke liye static export
+  output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
